0a7a5a29b5494dee38d7c2d429b5c19795ff04b8
vladCovaliov <vlad.covaliov@gmail.com>
2017-03-25 12:04:27 +0200

    Top Search bar Done

--- frontend/javascripts/Headers/WeatherHeaderContainer.jsx
+++ frontend/javascripts/Headers/WeatherHeaderContainer.jsx
@@ -1,41 +1,85 @@
 import { observer, inject } from 'mobx-react';
 
-@inject('cityStore')
+@inject('cityStore') @observer
 export default class WeatherHeaderContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hiddenCities: false
+    }
+  }
+
 	onSubmit = (event) => {
     event.preventDefault();
     this.refs.city.value.split(', ').forEach(
       city => this.props.cityStore.addCity(city));
     this.refs.city.value = '';
+    this.toggleHiddenCities();
   }
 
 	removeCity = (cityName, event) => {
     event.preventDefault();
+    event.stopPropagation();
     this.props.cityStore.removeCity(cityName);
+  }
+
+  stopPropagation = (event) => {
+    event.stopPropagation();
   }
 
 	getRemove(city) {
     return (
-      <a href="#" onClick={ this.removeCity.bind(this, city.name) } >
-        Remove
+      <a className="city-remove" onClick={ this.removeCity.bind(this, city.name) } >
+        <img src="/assets/x-icon.png" alt="Remove city icon"/>
       </a>
     );
   }
 
+  getCityInputs() {
+    const { values } = this.props.cityStore;
+
+    return values.map((city, index) =>
+      <div
+        className="city-input"
+        key={ index }
+      >
+        <span onClick={ this.stopPropagation }> { city.name } </span>
+        { this.getRemove(city) }
+      </div>
+    );
+  }
+
+  hideCities = () => {
+    this.setState({ hiddenCities: true });
+    this.refs.city.focus();
+  }
+
+  showCities = () => {
+    this.setState({ hiddenCities: false });
+  }
+
 	render() {
+    // We  can use classNames npm package instead
+    const classNames = ["cities-inputs"];
+    if (this.state.hiddenCities) {
+      classNames.push("hidden")
+    }
+
 		return (
 			<div className="w-header">
-		    <form onSubmit={ this.onSubmit }>
-		    <input
-		      ref="city"
-		      type="text"
-		    >
-		    </input>
-			    <button
-			      type="submit"
-			    >
-			      Search
-			    </button>
+		    <form onSubmit={ this.onSubmit } onFocus={ this.hideCities } onBlur={ this.showCities }>
+  		    <input
+  		      ref="city"
+  		      type="text"
+            onFocus={ this.hideCities } onBlur={ this.showCities }
+  		    >
+  		    </input>
+          <div
+            className={ classNames.join(', ') }
+            onClick={ this.hideCities }
+          >
+            { this.getCityInputs() }
+          </div>
 		    </form>
 		  </div>
 		)
